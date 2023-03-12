707ebde93b3bb1f46932605224caf8caf26011ca
BJ <95271342+DoctorBJones@users.noreply.github.com>
2023-03-12 14:44:53 +1100

    Adds tests

--- tests/testthat/test-create_dictionary.R
+++ tests/testthat/test-create_dictionary.R
@@ -1,14 +1,83 @@
 
 testthat::test_that("errors", {
+
   testthat::expect_error(
     create_dictionary(c(1,2,3,4)),
-
     "You can only make a dictionary for a dataframe or tibble"
   )
+
+  testthat::expect_error(
+    create_dictionary(iris, file = "test.csv"),
+    "You can only write to Excel files with extension `.xlsx`"
+  )
+
 })
 
+testthat::test_that("dictionary",{
 
-testthat::expect_equal(
-  nrow(create_dictionary(esoph)),
-  29
-)
+  # overall summary
+  over <- create_dictionary(
+    readRDS(file = testthat::test_path("testdata", 'tester_no_error.rds')),
+    id_var = "id")
+
+  testthat::expect_equal(over$summary[1],
+                         "Rows in dataset"
+  )
+
+  testthat::expect_equal(over$value[2],
+                         "14"
+  )
+
+  # dimensions of object
+  len <- create_dictionary(
+    readRDS(file = testthat::test_path("testdata", 'tester_no_error.rds')),
+    id_var = "id")
+
+  testthat::expect_equal(nrow(len),
+    62
+  )
+
+  # id var properly summarised
+  testthat::expect_equal(
+    len$label[3],
+    "Unique identifier"
+  )
+
+  # labelling working correctly
+
+  test_labels <- c(
+    id = "ID",
+    start_date = "Start date",
+    end_date = "End date",
+    gender = "Gender",
+    age = "Age",
+    state = "State",
+    duration = "Time taken to complete survey",
+    likert = "Agreement",
+    speed = "How fast",
+    suggestions = "Policy suggestions",
+    lab_location = "Location",
+    effective_date = "Date recorded",
+    all_missing = "Missing data",
+    time_recorded = "Time recorded"
+                   )
+
+  lab <- create_dictionary(
+    readRDS(file = testthat::test_path("testdata", 'tester_no_error.rds')),
+    id_var = "id", var_labels = test_labels)
+
+  testthat::expect_equal(
+    lab$label[5], "Start date"
+  )
+
+  # writing to Excel
+  xl <- create_dictionary(
+    readRDS(file = testthat::test_path("testdata", 'tester_no_error.rds')),
+    file = "test.xlsx")
+
+  testthat::test_path(
+    "~/test.xlsx"
+  )
+
+})
+
--- tests/testthat/test-summarise_variable.R
+++ tests/testthat/test-summarise_variable.R
@@ -1,9 +1,187 @@
-testthat::expect_length(
-  summarise_variable(iris, "Species"),
-  5
-)
+testthat::test_that("length", {
 
-testthat::expect_equal(
-  nrow(summarise_variable(iris, "Sepal.Length")),
-  5
-)
+  testthat::expect_length(
+    summarise_variable(
+      readRDS(file = testthat::test_path("testdata", 'tester.rds')),
+      "start_date"),
+    5
+  )
+
+  testthat::expect_equal(
+    nrow(summarise_variable(
+      readRDS(file = testthat::test_path("testdata", 'tester.rds')),
+      "lab_location")),
+    3
+  )
+
+})
+
+# Errors
+
+testthat::test_that("error", {
+
+  testthat::expect_warning(
+    summarise_variable(
+      readRDS(file = testthat::test_path("testdata", 'tester.rds')),
+      "bad_factor"),
+    "bad_factor has more than 10 levels, did you want a character variable?"
+  )
+})
+
+# test each data class
+testthat::test_that("classes", {
+
+  #'Date' class
+  sd <- summarise_variable(
+    readRDS(file = testthat::test_path("testdata",'tester.rds')),
+    "start_date")
+
+  testthat::expect_equal(
+    sd$value[1],
+    "2022-02-17"
+  )
+
+  # POSIX date class
+  ed <- summarise_variable(
+    readRDS(file = testthat::test_path("testdata", 'tester.rds')),
+    "end_date")
+
+  testthat::expect_equal(
+    ed$value[2],
+    "2022-01-20 2022-04-18 2022-04-22"
+  )
+
+  # factor class
+  g <- summarise_variable(
+    readRDS(file = testthat::test_path("testdata", 'tester.rds')),
+    "gender")
+
+  testthat::expect_equal(
+    g$summary[1],
+    "Female (1)"
+  )
+
+  # integer
+  a <- summarise_variable(
+    readRDS(file = testthat::test_path("testdata", 'tester.rds')),
+    "age")
+
+  testthat::expect_equal(
+    a$value[4],
+    "49"
+  )
+
+  # haven labelled
+  s <- summarise_variable(
+    readRDS(file = testthat::test_path("testdata", 'tester.rds')),
+    "state")
+
+  testthat::expect_equal(
+    s$summary[3],
+    "Vic (4)"
+  )
+
+  # difftime
+  d <- summarise_variable(
+    readRDS(file = testthat::test_path("testdata", 'tester.rds')),
+    "duration")
+
+  testthat::expect_equal(
+    d$value[2],
+    "20"
+  )
+
+  # ordered factor
+  l <- summarise_variable(
+    readRDS(file = testthat::test_path("testdata", 'tester.rds')),
+    "likert")
+
+  testthat::expect_equal(
+    l$summary[3],
+    "Disagree (3)"
+  )
+
+  # double
+  d <- summarise_variable(
+    readRDS(file = testthat::test_path("testdata", 'tester.rds')),
+    "speed")
+
+  testthat::expect_equal(
+    d$value[3],
+    "3.83"
+  )
+
+  # character
+
+  testthat::expect_warning(
+    summarise_variable(readRDS(file = testthat::test_path("testdata",'tester.rds')), "comments"),
+    "comments has fewer than 10 unique values, did you want a factor?"
+  )
+
+  c  <- summarise_variable(
+    readRDS(file = testthat::test_path("testdata", 'tester.rds')),
+    "suggestions")
+  testthat::expect_equal(
+    c$summary,
+    c("unique responses", "missing")
+  )
+
+  # logical
+  log <- summarise_variable(
+    readRDS(file = testthat::test_path("testdata", 'tester.rds')),
+    "lab_location")
+
+  testthat::expect_equal(
+    log$summary[2],
+    "TRUE"
+  )
+
+  # datetime
+  dttm <- summarise_variable(
+    readRDS(file = testthat::test_path("testdata", 'tester.rds')),
+    "effective_date")
+
+  testthat::expect_equal(
+    dttm$value[1],
+    "2022-04-13"
+  )
+
+  # all missing values
+  nas <- summarise_variable(
+    readRDS(file = testthat::test_path("testdata", 'tester.rds')),
+    "all_missing")
+
+  testthat::expect_equal(
+    nas$value[1],
+    11
+  )
+
+  # times
+  time <- summarise_variable(
+    readRDS(file = testthat::test_path("testdata", 'tester.rds')),
+    "time_recorded")
+
+  testthat::expect_equal(
+    time$value[4],
+    "21:36:52"
+  )
+
+})
+
+testthat::test_that("NA and mode", {
+
+  testthat::expect_equal(
+    nrow(summarise_variable(
+      readRDS(file = testthat::test_path("testdata", 'tester.rds')),
+      "all_missing")),
+    1
+  )
+
+  tester <- readRDS(file = testthat::test_path("testdata", 'tester.rds'))
+
+  testthat::expect_equal(
+    mode_stat(tester$start_date),
+    c(18996, 19084)
+  )
+
+})
